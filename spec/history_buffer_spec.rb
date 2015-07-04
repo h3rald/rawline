020a8d8d61229eacf66165faaebf5fc6c0ef736f
Zach Dennis <zach.dennis@gmail.com>
2015-07-03 21:28:08 -0400

    Updating to RSpec 3 expect style syntax. No spec changes.

--- spec/history_buffer_spec.rb
+++ spec/history_buffer_spec.rb
@@ -9,7 +9,7 @@ describe RawLine::HistoryBuffer do
 	end
 
 	it "instantiates an empty array when created" do
-		@history.length.should == 0
+		expect(@history.length).to eq(0)
 	end
 
 	it "allows items to be added to the history" do
@@ -18,15 +18,15 @@ describe RawLine::HistoryBuffer do
 		@history << "line #2"
 		@history << "line #3"
 		@history << "line #2"
-		@history.should == ["line #1", "line #3", "line #2"]
+		expect(@history).to eq(["line #1", "line #3", "line #2"])
 		@history.duplicates = true
 		@history << "line #3"
-		@history.should == ["line #1", "line #3", "line #2", "line #3"]
+		expect(@history).to eq(["line #1", "line #3", "line #2", "line #3"])
 		@history.exclude = lambda { |i| i.match(/line #[456]/) }
 		@history << "line #4"
 		@history << "line #5"
 		@history << "line #6"
-		@history.should == ["line #1", "line #3", "line #2", "line #3"]	
+		expect(@history).to eq(["line #1", "line #3", "line #2", "line #3"])
 	end
 
 	it "does not overflow" do
@@ -36,13 +36,13 @@ describe RawLine::HistoryBuffer do
 		@history << "line #4"
 		@history << "line #5"
 		@history << "line #6"
-		@history.length.should == 5
+		expect(@history.length).to eq(5)
 	end
 
 	it "allows navigation back and forward" do
 		@history.back
 		@history.forward
-		@history.position.should == nil
+		expect(@history.position).to eq(nil)
 		@history << "line #1"
 		@history << "line #2"
 		@history << "line #3"
@@ -53,36 +53,36 @@ describe RawLine::HistoryBuffer do
 		@history.back
 		@history.back
 		@history.back
-		@history.position.should == 0
+		expect(@history.position).to eq(0)
 		@history.back
-		@history.position.should == 0
+		expect(@history.position).to eq(0)
 		@history.forward
-		@history.position.should == 1
-		@history.forward
-		@history.forward
+		expect(@history.position).to eq(1)
 		@history.forward
 		@history.forward
-		@history.position.should == 4
 		@history.forward
-		@history.position.should == 4
+		@history.forward
+		expect(@history.position).to eq(4)
+		@history.forward
+		expect(@history.position).to eq(4)
 		@history.cycle = true
 		@history.forward
 		@history.forward
-		@history.position.should == 1
+		expect(@history.position).to eq(1)
 	end
 
 	it "can retrieve the last element or the element at @position via 'get'" do
-		@history.get.should == nil
+		expect(@history.get).to eq(nil)
 		@history << "line #1"
 		@history << "line #2"
 		@history << "line #3"
 		@history << "line #4"
 		@history << "line #5"
-		@history.get.should == "line #5"
+		expect(@history.get).to eq("line #5")
 		@history.back
-		@history.get.should == "line #4"
+		expect(@history.get).to eq("line #4")
 		@history.forward
-		@history.get.should == "line #5"
+		expect(@history.get).to eq("line #5")
 	end
 
 	it "can be cleared and resized" do
@@ -93,14 +93,14 @@ describe RawLine::HistoryBuffer do
 		@history << "line #5"
 		@history.back
 		@history.back
-		@history.get.should == "line #4"
+		expect(@history.get).to eq("line #4")
 		@history.resize(6)
-		@history.position.should == nil
+		expect(@history.position).to eq(nil)
 		@history << "line #6"
-		@history.get.should == "line #6"
+		expect(@history.get).to eq("line #6")
 		@history.empty
-		@history.should == []
-		@history.size.should == 6
-		@history.position.should == nil
+		expect(@history).to eq([])
+		expect(@history.size).to eq(6)
+		expect(@history.position).to eq(nil)
 	end
 end
